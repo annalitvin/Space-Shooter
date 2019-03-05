28c0a452fa63e9e222e23ca7eb655ec1524c5d48
annalitvin <litvin_any@ukr.net>
2019-03-06 01:24:03 +0200

    set the settings for the game

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings():
+    """Класс для хранения всех настроек игры Alient Invasion"""
+
+    def __init__(self):
+        """Инициализирует настройки игры"""
+        self.screen_width = 1200
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
