62891b882a3085d7ea4ac3e1506bdd1343185b85
annalitvin <litvin_any@ukr.net>
2019-03-08 02:48:24 +0200

    ship relocation implemented

--- alien_invasion.py
+++ alien_invasion.py
@@ -18,7 +18,8 @@ def run_game():
 
     #Запуск основного цикла игры
     while True:
-        gf.check_events()
+        gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings, screen, ship)
         screen.fill(ai_settings.bg_color)
         ship.blitme()
--- ship.py
+++ ship.py
@@ -4,13 +4,27 @@ class Ship():
     def __init__(self, screen):
         """Инициализирует корабль и задает его начальную позицию"""
         self.screen = screen
+
         #Загрузка изображения корабля и получение прямоугольника
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
+
         #Каждый новый корабль появляется у нижнего края экрана
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+
+        # Флаг перемещения
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Обновление позиции корабля с учетом флага"""
+
+        if self.moving_right:
+            self.rect.centerx += 1
+        elif self.moving_left:
+            self.rect.centerx -= 1
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
