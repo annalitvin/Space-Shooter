c8317dc9c86ddd5dd003e370089e65f18950398f
annalitvin <litvin_any@ukr.net>
2019-03-10 23:25:52 +0200

    alien object created

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 def run_game():
@@ -20,12 +21,15 @@ def run_game():
     # Создание группы для хранения пуль.
     bullets = Group()
 
+    # Создание пришельца.
+    alien = Alien(ai_settings, screen)
+
     #Запуск основного цикла игры
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, alien, bullets)
         screen.fill(ai_settings.bg_color)
         ship.blitme()
 
